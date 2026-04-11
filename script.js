289c4d82ad5ff914b30048d12fe2f4f160cba5f6
umairDevoloper-8712 <umairarshad8712@gmail.com>
2026-04-11 16:21:07 +0600

    Add files via upload
    thired portfolio website

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+  $("#owl-demo").owlCarousel({
+
+    items: 1,
+    navigation: false,
+    pagination: true,
+    autoPlay: 3000,
+    stopOnHover: true,
+    slideSpeed: 800,
+    paginationSpeed: 800
+
+  });
+
+});
